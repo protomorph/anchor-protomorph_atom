735a1cbcf47786a8156472cf099f08fa3ec6fe1b
protomorph <protomorph@users.noreply.github.com>
2014-10-24 14:33:28 +0100

    better readability

--- protomorph_atom/js/style.js
+++ protomorph_atom/js/style.js
@@ -11,13 +11,17 @@
 
 	// DROPDOWN MENU
 	// =============
+
 	$('[data-toggle="dropdown"]').on('click touchstart', function(e) {
-		$(this).parent('.dropdown').toggleClass('open');
+		$(this).parent('.dropdown')
+			.toggleClass('open');
 
 		if (!$(this).parent('.dropdown').hasClass('open')) {
-			$(this).next('.dropdown-menu').slideUp(300);
+			$(this).next('.dropdown-menu')
+				.slideUp(300);
 		} else {
-			$(this).next('.dropdown-menu').slideDown(300);
+			$(this).next('.dropdown-menu')
+				.slideDown(300);
 		}
 
 		e.preventDefault();
@@ -35,6 +39,7 @@
 
 	// FLOAT LABELS
 	// ============
+
 	$('html, body').find('.float-input').each(function() {
 		$(this).on('check-value', function() {
 			var _label = $(this).next('.float-label');
@@ -44,17 +49,22 @@
 			} else {
 				_label.removeClass('in');
 			}
-		}).on('keyup', function() {
+		})
+		.on('keyup', function() {
 			$(this).trigger('check-value');
-		}).on('input propertychange', function() {
+		})
+		.on('input propertychange', function() {
 			$(this).trigger('check-value');
-		}).on('textarea propertychange', function() {
+		})
+		.on('textarea propertychange', function() {
 			$(this).trigger('check-value');
-		}).trigger('check-value');
+		})
+		.trigger('check-value');
 	});
 
 	// OFF CANVAS NAVBAR
 	// =================
+
 	$('[data-toggle="off-canvas"]').on('click touchstart', function() {
 		$('.page-body').toggleClass('in');
 	});
@@ -69,14 +79,19 @@
 	// =============
 	$(window).on('scroll', function() {
 		if ($(this).scrollTop() > 200) {
-			$('.scroll-up').stop(true, true).fadeIn(300);
+			$('.scroll-up').stop(true, true)
+				.fadeIn(300);
 		} else if ($(this).scrollTop() <= 200) {
-			$('.scroll-up').stop(true, true).fadeOut(300);
+			$('.scroll-up').stop(true, true)
+				.fadeOut(300);
 		}
 	});
 
 	$('.scroll-up').on('click touchstart', function(e) {
-		$('html, body').animate({scrollTop: 0}, 600, 'swing');
+		$('html, body').animate({scrollTop: 0},
+			600,
+			'swing'
+		);
 
 		e.preventDefault();
 	});
