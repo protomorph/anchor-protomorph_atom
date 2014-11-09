e913d31c5d24962fb30d21a08132e2c60c871e5b
protomorph <protomorph@users.noreply.github.com>
2014-11-09 23:40:21 +0000

    updated

--- protomorph_atom/css/style.css
+++ protomorph_atom/css/style.css
@@ -1,6 +1,6 @@
 /* =======================================================================
  * Protomorph Atom: Version 1.0.0
- * @ProtoMorph (http://protomorph.tk/)
+ * @ProtoMorph (http://protomorph.cf/)
  * =======================================================================
  * Copyright 2014
  * Licensed GNU General Public License, version 2 (GPL-2.0)
